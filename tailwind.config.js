d9ce80d4d9131154c0ccdc527b0376b4ceb09bb9
micaelabenitez <mabenittez@gmail.com>
2024-11-26 21:46:46 -0300

    add title animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,25 @@ module.exports = {
       fontFamily: {
         raleway: ['var(--font-raleway)'],
       },
-
+      keyframes: {
+        "zoom-in-down": {
+            "0%": {
+              opacity: 0,
+              transform: "scale3d(0.3, 0.3, 0.3) translate3d(0, -100%, 0)",
+            },
+            "80%": {
+              opacity: 0.8,
+              transform: "scale3d(1.1, 1.1, 1.1)",
+            },
+            "100%": {
+              opacity: 1,
+              transform: "translate3d(0, 0%, 0)"
+            },
+          },
+      },
+      animation: {
+        zoomInDown: 'zoom-in-down 1s ease-in-out 0.10s 1',
+      },
     },
   },
   plugins: [],
