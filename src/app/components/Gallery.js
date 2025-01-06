5edb10ea85d5f432d317a9a776184c91e50e043c
micaelabenitez <mabenittez@gmail.com>
2025-01-06 00:27:14 -0300

    fix

--- src/app/components/Gallery.js
+++ src/app/components/Gallery.js
@@ -3,7 +3,7 @@ import img from "../s2.webp"
 
 export default function Gallery() {
     return (
-        <div className="w-full h-[33rem] gap-8 flex flex-shrink-0 overflow-hidden">
+        <div className="hidden md:w-full md:h-[33rem] md:gap-8 md:flex md:flex-shrink-0 md:overflow-hidden">
             <div className="flex gap-8 items-center flex-shrink-0 w-full h-full animate-loopScroll">
                 <Image src={img} alt="" className="bg-orange-500 w-[40rem] h-[33rem]"/>
                 <Image src={img} alt="" className="bg-orange-500 w-[40rem] h-[33rem]"/>
